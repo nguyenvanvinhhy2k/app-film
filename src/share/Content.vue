f7ba35f1c6bb8cd468963f6da474aa5bd0df9d53
Vinh Nguyen <nguyenvanvinhhy2k@gmail.com>
2023-12-21 16:23:24 +0700

    update

--- src/share/Content.vue
+++ src/share/Content.vue
@@ -1,11 +1,56 @@
 <template>
-  <div class=" relative flex max-w-[1920px] mx-auto text-[#ccc] mt-[20px]">
-  <div id="app" class="text-[#4f4f4f] w-full xl:max-w-[calc(100%-160px)] 2xl:max-w-[calc(100%-500px)] mx-auto px-[16px] xl:px-0 lg:justify-between flex justify-between items-center">
-    <div class="py-[15px] flex items-center justify-between w-full text-[16px]">
+  <div class="relative flex max-w-[1920px] mx-auto text-[#ccc] mt-[40px]">
+    <div id="app" class="text-[#4f4f4f] w-full xl:max-w-[calc(100%-250px)] mx-auto px-[16px] xl:px-0 lg:justify-between flex justify-between items-center">
+      <div class="flex justify-between w-full text-[16px]">
+        <div class="w-[69%] flex justify-between">
+          <div class="w-[30%] bg-[#f5f6f7] rounded-sm px-[15px] font-medium">
+            <div class="border-b-[1px] border-[#e2e2e2] py-[15px]">
+              Sao Chelsea lên tiếng xin lỗi sau sai lầm trước Newcastle
+            </div>
+            <div class="border-b-[1px] border-[#e2e2e2] py-[15px]">
+              Sao Chelsea lên tiếng xin lỗi sau sai lầm trước Newcastle
+            </div>
+            <div class="border-b-[1px] border-[#e2e2e2] py-[15px]">
+              Sao Chelsea lên tiếng xin lỗi sau sai lầm trước Newcastle
+            </div>
+            <div class="border-b-[1px] border-[#e2e2e2] py-[15px]">
+              Sao Chelsea lên tiếng xin lỗi sau sai lầm trước Newcastle
+            </div>
+            <div class="border-b-[1px] border-[#e2e2e2] py-[15px]">
+              Sao Chelsea lên tiếng xin lỗi sau sai lầm trước Newcastle
+            </div>
+            <div class="border-b-[1px] border-[#e2e2e2] py-[15px]">
+              Sao Chelsea lên tiếng xin lỗi sau sai lầm trước Newcastle
+            </div>
+            <div class="border-b-[1px] border-[#e2e2e2] py-[15px]">
+              Sao Chelsea lên tiếng xin lỗi sau sai lầm trước Newcastle
+            </div>
+          </div>
 
-      content
+          <div class="w-[68%]">
+            <img class="rounded-lg" src="https://vncdn.sabasports.com/2023/12/Ket-qua-NBA-14.jpg" alt="" />
+            <p class="font-bold mt-[20px] text-[28px] mb-[10px]">
+              Kết quả NBA hôm nay ngày 21/12: LA Clippers tiếp tục bay cao,
+              Lakers lại chuốc thất bại
+            </p>
+            <p class="text-[20px]">
+              SABAVN cập nhật kết quả NBA hôm nay, video highlights các trận đấu
+              hấp dẫn tại giải bóng rổ Nhà nghề Mỹ diễn ra vào thứ 5 ngày 21/12.
+            </p>
+          </div>
+        </div>
+
+        <div class="w-[29%]">
+              <div class="relative mb-[60px] hidden lg:block">
+              
+      <div class="px-[22px] py-[8px] bg-[#f12f19] rounded-t-lg text-[#fff] text-[18px] font-bold absolute">
+        LỊCH THI ĐẤU
+      </div>
+           <div class="w-full h-[3px] bg-[#f12f19] absolute top-[42px]" />
     </div>
-  </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/share/Footer.vue
+++ src/share/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class=" relative flex max-w-[1920px] mx-auto mt-[20px] bg-[#565759]">
-  <div id="app" class="text-[#4f4f4f] w-full xl:max-w-[calc(100%-160px)] 2xl:max-w-[calc(100%-500px)] mx-auto px-[16px] xl:px-0 lg:justify-between flex justify-between items-center">
+  <div id="app" class="text-[#4f4f4f] w-full xl:max-w-[calc(100%-250px)] mx-auto px-[16px] xl:px-0 lg:justify-between flex justify-between items-center">
     <div class=" text-[#fff] py-[20px] text-[16px] font-bold"> footer</div>
 </div>
 </div>
