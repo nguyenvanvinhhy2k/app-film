d6591d5fe72d2247d38cf38723dd419b55279456
Vinh Nguyen <nguyenvanvinhhy2k@gmail.com>
2023-12-22 13:45:50 +0700

    update

--- src/share/Content.vue
+++ src/share/Content.vue
@@ -5,8 +5,9 @@
       class="text-[#4f4f4f] w-full xl:max-w-[calc(100%-250px)] mx-auto px-[16px] xl:px-0 lg:justify-between flex justify-between items-center"
     >
       <div class="flex justify-between w-full text-[16px]">
-        <div class="w-[69%] flex justify-between">
-          <div class="w-[30%] bg-[#f5f6f7] rounded-sm px-[15px] font-medium">
+        <div class="w-[69%]">
+          <div class=" flex justify-between">
+          <div class="w-[30%] bg-[#f5f6f7] rounded-sm px-[15px] font-medium h-fit">
             <div class="border-b-[1px] border-[#e2e2e2] py-[15px]">
               Sao Chelsea lên tiếng xin lỗi sau sai lầm trước Newcastle
             </div>
@@ -25,7 +26,7 @@
             <div class="border-b-[1px] border-[#e2e2e2] py-[15px]">
               Sao Chelsea lên tiếng xin lỗi sau sai lầm trước Newcastle
             </div>
-            <div class="border-b-[1px] border-[#e2e2e2] py-[15px]">
+            <div class=" py-[15px]">
               Sao Chelsea lên tiếng xin lỗi sau sai lầm trước Newcastle
             </div>
           </div>
@@ -45,10 +46,124 @@
               hấp dẫn tại giải bóng rổ Nhà nghề Mỹ diễn ra vào thứ 5 ngày 21/12.
             </p>
           </div>
+          </div>
+
+          <div class="mt-[30px] flex justify-between">
+            <div class="w-[32%]"><img class="rounded-lg h-[210px] w-full" src="https://vncdn.sabasports.com/2023/12/Aston-Villa-dan-dau-Ngoai-hang-Anh-2.webp" alt=""> <p class="mt-[10px] text-[20px] font-bold">Unai Emery nói về Super League và cơ hội để Aston Villa dẫn đầu Ngoại hạng Anh </p></div>
+             <div class="w-[32%]"><img class="rounded-lg h-[210px] w-full" src="https://vncdn.sabasports.com/2023/12/Tin-nong-bong-da-quoc-te-hom-nay-22-12-3.jpg" alt=""> <p class="mt-[10px] text-[20px] font-bold">Unai Emery nói về Super League và cơ hội để Aston Villa dẫn đầu Ngoại hạng Anh </p></div>
+              <div class="w-[32%]"><img class="rounded-lg h-[210px] w-full" src="https://vncdn.sabasports.com/2023/12/Iraq-Viet-Nam-Asian-Cup-1.jpg" alt=""> <p class="mt-[10px] text-[20px] font-bold">Unai Emery nói về Super League và cơ hội để Aston Villa dẫn đầu Ngoại hạng Anh </p></div>
+          </div>
+
+          <div class="mt-[40px]"><img class="w-full rounded-lg" src="https://www.sabavn.com//uploads/banner/457WEB910x150.png" alt=""></div>
+
+          <div class="flex justify-between mt-[40px]">
+            <div class="w-[31%]">
+              <div class="relative mb-[50px] ">
+            <div
+              class="px-[22px] py-[8px] bg-[#f12f19] rounded-t-lg text-[#fff] text-[16px] font-bold absolute"
+            >
+              ASIAN CUP
+            </div>
+            <div class="w-full h-[3px] bg-[#f12f19] absolute top-[38px]" />
+              </div>
+
+              <div class="mt-[10px] overflow-hidden">
+            <img class="rounded-lg h-[210px] " src="https://vncdn.sabasports.com/2023/12/BLV-Quang-Huy-Dt-Viet-Nam-asian-cup-1.jpg" alt="">
+
+            <p class="text-[22px] font-bold mt-[10px]">BLV Quang Huy nêu 3 ngôi sao giúp ĐT Việt Nam tấn công đẹp mắt tại Asian Cup</p>
+            <p>23 giờ trước</p>
+          </div>
+
+          <div class="mt-[10px] border-t-[1px] border-[#e2e2e2]">
+             <p class="font-medium text-[18px] mt-[10px]">BLV Quang Huy nêu 3 ngôi sao giúp ĐT Việt Nam tấn công đẹp mắt tại Asian Cup</p>
+            <p>23 giờ trước</p>
+          </div>
+
+           <div class="mt-[10px] border-t-[1px] border-[#e2e2e2]">
+             <p class="font-medium text-[18px] mt-[10px]">BLV Quang Huy nêu 3 ngôi sao giúp ĐT Việt Nam tấn công đẹp mắt tại Asian Cup</p>
+            <p>23 giờ trước</p>
+          </div>
+
+           <div class="mt-[10px] border-t-[1px] border-[#e2e2e2]">
+             <p class="font-medium text-[18px] mt-[10px]">BLV Quang Huy nêu 3 ngôi sao giúp ĐT Việt Nam tấn công đẹp mắt tại Asian Cup</p>
+            <p>23 giờ trước</p>
+          </div>
+            </div>
+
+            <div class="w-[31%]">
+                 <div class="relative mb-[50px] ">
+            <div
+              class="px-[22px] py-[8px] bg-[#f12f19] rounded-t-lg text-[#fff] text-[16px] font-bold absolute"
+            >
+              NGOẠI HẠNG ANH
+            </div>
+            <div class="w-full h-[3px] bg-[#f12f19] absolute top-[38px]" />
+          </div>
+
+           <div class="mt-[10px]">
+            <img class="rounded-lg  h-[210px] 
+            " src="https://vncdn.sabasports.com/2023/12/Aston-Villa-dan-dau-Ngoai-hang-Anh-2.webp" alt="">
+
+            <p class="text-[22px] font-bold mt-[10px]">BLV Quang Huy nêu 3 ngôi sao giúp ĐT Việt Nam tấn công đẹp mắt tại Asian Cup</p>
+            <p>23 giờ trước</p>
+          </div>
+
+          <div class="mt-[10px] border-t-[1px] border-[#e2e2e2]">
+             <p class="font-medium text-[18px] mt-[10px]">BLV Quang Huy nêu 3 ngôi sao giúp ĐT Việt Nam tấn công đẹp mắt tại Asian Cup</p>
+            <p>23 giờ trước</p>
+          </div>
+
+           <div class="mt-[10px] border-t-[1px] border-[#e2e2e2]">
+             <p class="font-medium text-[18px] mt-[10px]">BLV Quang Huy nêu 3 ngôi sao giúp ĐT Việt Nam tấn công đẹp mắt tại Asian Cup</p>
+            <p>23 giờ trước</p>
+          </div>
+
+           <div class="mt-[10px] border-t-[1px] border-[#e2e2e2]">
+             <p class="font-medium text-[18px] mt-[10px]">BLV Quang Huy nêu 3 ngôi sao giúp ĐT Việt Nam tấn công đẹp mắt tại Asian Cup</p>
+            <p>23 giờ trước</p>
+          </div>
+            </div>
+
+            <div class="w-[31%]">
+                 <div class="relative mb-[50px] ">
+            <div
+              class="px-[22px] py-[8px] bg-[#f12f19] rounded-t-lg text-[#fff] text-[16px] font-bold absolute"
+            >
+              V-LEAGUE
+            </div>
+            <div class="w-full h-[3px] bg-[#f12f19] absolute top-[38px]" />
+          </div>
+
+           <div class="mt-[10px]">
+            <img class="rounded-lg  h-[210px] 
+            " src="https://vncdn.sabasports.com/2023/12/truc-tiep-bong-da-binh-dinh-vs-viettel-5.jpg" alt="">
+
+            <p class="text-[22px] font-bold mt-[10px]">BLV Quang Huy nêu 3 ngôi sao giúp ĐT Việt Nam tấn công đẹp mắt tại Asian Cup</p>
+            <p>23 giờ trước</p>
+          </div>
+
+          <div class="mt-[10px] border-t-[1px] border-[#e2e2e2]">
+             <p class="font-medium text-[18px] mt-[10px]">BLV Quang Huy nêu 3 ngôi sao giúp ĐT Việt Nam tấn công đẹp mắt tại Asian Cup</p>
+            <p>23 giờ trước</p>
+          </div>
+
+           <div class="mt-[10px] border-t-[1px] border-[#e2e2e2]">
+             <p class="font-medium text-[18px] mt-[10px]">BLV Quang Huy nêu 3 ngôi sao giúp ĐT Việt Nam tấn công đẹp mắt tại Asian Cup</p>
+            <p>23 giờ trước</p>
+          </div>
+
+           <div class="mt-[10px] border-t-[1px] border-[#e2e2e2]">
+             <p class="font-medium text-[18px] mt-[10px]">BLV Quang Huy nêu 3 ngôi sao giúp ĐT Việt Nam tấn công đẹp mắt tại Asian Cup</p>
+            <p>23 giờ trước</p>
+          </div>
+            </div>
+          </div>
+
+
         </div>
 
         <div class="w-[29%]">
-          <div class="relative mb-[50px] hidden lg:block">
+          <div class="relative mb-[50px] ">
             <div
               class="px-[22px] py-[8px] bg-[#f12f19] rounded-t-lg text-[#fff] text-[16px] font-bold absolute"
             >
@@ -87,6 +202,44 @@
               </div>
             </div>
           </div>
+
+            <div class="relative pb-[50px] mt-[30px] ">
+            <div
+              class="px-[22px] py-[8px] bg-[#f12f19] rounded-t-lg text-[#fff] text-[16px] font-bold absolute"
+            >
+              TIN TIÊU ĐIỂM
+            </div>
+            <div class="w-full h-[3px] bg-[#f12f19] absolute top-[38px]" />
+          </div>
+
+          <div class="mt-[10px]">
+            <img class="rounded-lg ov
+            " src="https://vncdn.sabasports.com/2023/12/BLV-Quang-Huy-Dt-Viet-Nam-asian-cup-1.jpg" alt="">
+
+            <p class="text-[22px] font-bold mt-[10px]">BLV Quang Huy nêu 3 ngôi sao giúp ĐT Việt Nam tấn công đẹp mắt tại Asian Cup</p>
+            <p>23 giờ trước</p>
+          </div>
+
+          <div class="mt-[10px] border-t-[1px] border-[#e2e2e2]">
+             <p class="font-medium text-[18px] mt-[10px]">BLV Quang Huy nêu 3 ngôi sao giúp ĐT Việt Nam tấn công đẹp mắt tại Asian Cup</p>
+            <p>23 giờ trước</p>
+          </div>
+
+           <div class="mt-[10px] border-t-[1px] border-[#e2e2e2]">
+             <p class="font-medium text-[18px] mt-[10px]">BLV Quang Huy nêu 3 ngôi sao giúp ĐT Việt Nam tấn công đẹp mắt tại Asian Cup</p>
+            <p>23 giờ trước</p>
+          </div>
+
+           <div class="mt-[10px] border-t-[1px] border-[#e2e2e2]">
+             <p class="font-medium text-[18px] mt-[10px]">BLV Quang Huy nêu 3 ngôi sao giúp ĐT Việt Nam tấn công đẹp mắt tại Asian Cup</p>
+            <p>23 giờ trước</p>
+          </div>
+
+          <div class="mt-[40px]"><img class="w-full ob" src="https://www.sabavn.com/uploads/banner/683350x490.jpg" alt=""></div>
+
+
+
+
         </div>
       </div>
     </div>
